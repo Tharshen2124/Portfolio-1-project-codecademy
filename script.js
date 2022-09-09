54722fa19af63d26a2d8a7a0e93439e878e9c20e
Tharshen <tharshensurianbalan@gmail.com>
2022-09-09 11:23:53 +0800

    finished up on the making the sentence display correctly

---
+++ script.js
@@ -0,0 +1,40 @@
+let word1 = ['The boys', 'We', 'They', "The girls"];
+let word2 = ['love', 'hate', 'prefer'];
+let word3 = ['playing', 'cooking', 'eating'];
+
+
+function getFirstWord (word1) {
+    const firstWord = word1[Math.floor(Math.random()*4)];
+    return firstWord;
+}
+
+const getSecondWord = word2 => {
+    const secondWord = word2[Math.floor(Math.random()*2)];
+    return secondWord;
+}
+
+const getThirdWord = word3 => word3[Math.floor(Math.random()*2)];
+
+const ThirdWord = getThirdWord(word3);
+
+const getLastWord = ThirdWord => {
+    let lastWord;
+    let choice1 = ['football', 'rugby', 'basketball'];
+    let choice2 = ['burgers', 'french fries', 'fried chicken'];
+    if(ThirdWord === 'playing') {
+        lastWord = choice1[Math.floor(Math.random()*2)];
+    } else if (ThirdWord === 'cooking' || ThirdWord === 'eating') { 
+        lastWord = choice2[Math.floor(Math.random()*2)];
+    }
+
+    return lastWord;
+}
+
+const arrayForSentence = [getFirstWord(word1), getSecondWord(word2), ThirdWord, getLastWord(ThirdWord)];
+
+let sentenceFix1 = arrayForSentence.toString();
+
+let sentenceFix2 = sentenceFix1.replace(/,/g, ' ');
+
+console.log(sentenceFix2);
+
